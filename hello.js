58629b756aa9bb3510aa3ca7d70e3a654b3a8833
AVIGAIL120 <c5715345@gmail.com>
2025-11-17 23:13:33 +0200

    change

--- hello.js
+++ hello.js
@@ -2,15 +2,12 @@ const a =()=>{
 
 }
 const b =()=>{
-<<<<<<< HEAD
     return 5
 }
 const e =()=>{
     alert("e")
-=======
     return '5'
 }
 const c =()=>{
     console.log("function c")
->>>>>>> master
 }
